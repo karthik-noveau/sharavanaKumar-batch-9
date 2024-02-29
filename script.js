a2abdec68f6a02137b8e52eb9618b0116465a5d7
Sharavana Kumar <158765639+Sharavanakumar-Ramalingam@users.noreply.github.com>
2024-02-29 21:24:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+        }
+    });
+};
+// Smooth scrolling for anchor links
+document.querySelectorAll('header nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 50, // Adjusted offset for header height
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+// Scroll reavaeal
+ScrollReveal({
+     reset: true,
+     distance:'80px',
+     duration:2000,
+     delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-image, .project-container, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-image', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
